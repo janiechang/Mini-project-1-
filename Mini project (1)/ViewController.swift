4a6acf53a3763739caa92c8a1b6c93a8a28b5157
janiechang <janiechang0425@gmail.com>
2022-06-14 13:50:52 -0400

    Initial Commit

---
+++ Mini project (1)/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Mini project (1)
+//
+//  Created by Janie Chang on 8/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainTitle: UILabel!
+    
+    @IBOutlet weak var factOne: UILabel!
+    
+    @IBOutlet weak var factTwo: UILabel!
+  
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var bonusFact: UILabel!
+    
+    @IBOutlet weak var bonusImage: UIImageView!
+
+    @IBAction func button(_ sender: UIButton) {       bonusImage.isHidden = false
+        bonusFact.isHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        bonusFact.isHidden = true
+        bonusImage.isHidden = true
+    }
+}
---
+++ Mini project (1)UITests/Mini_project__1_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Mini_project__1_UITestsLaunchTests.swift
+//  Mini project (1)UITests
+//
+//  Created by Janie Chang on 8/6/22.
+//
+
+import XCTest
+
+class Mini_project__1_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
